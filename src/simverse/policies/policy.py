4193e7655749c1f44cf5561a7be765afee2f0daf
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-20 00:02:10 +0530

    policy is just a pytorch module

--- src/simverse/policies/policy.py
+++ src/simverse/policies/policy.py
@@ -1,5 +1,14 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from torch.nn import Module
+import torch
 
-class Policy(ABC):
-    pass
+class Policy(ABC, Module):
+    @abstractmethod
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def forward(self, *args, **kwargs) -> torch.Tensor:
+        pass
+
