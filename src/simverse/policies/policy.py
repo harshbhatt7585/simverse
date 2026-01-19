34dd3616225a9c755af3a4dbcd8ebd12e334cdf7
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-19 23:55:55 +0530

    not sure about policy rn

--- src/simverse/policies/policy.py
+++ src/simverse/policies/policy.py
@@ -2,10 +2,4 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class Policy(ABC):
-    @abstractmethod
-    def __init__(self, *args, **kwargs) -> None:
-        pass
-
-    @abstractmethod
-    def act(self, obs: np.ndarray) -> np.ndarray:
-        pass
+    pass
