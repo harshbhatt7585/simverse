68c69412ce14d6eedbdd9b31bf2a6e015a8c3a55
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-22 18:25:09 +0530

    define farmtila config

---
+++ src/simverse/envs/farmtila/config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class FarmtilaConfig:
+    width: int = 50
+    height: int = 50
+    num_agents: int = 5
+    spawn_seed_every: int = 100
+    seeds_per_spawn: int = 10
+    max_steps: int = 10000
