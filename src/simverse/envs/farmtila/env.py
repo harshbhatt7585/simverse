5d6a13dd09d7352163b6d0638be878a266671e1a
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-22 18:46:24 +0530

    initialize the farmtila env class

---
+++ src/simverse/envs/farmtila/env.py
@@ -0,0 +1,27 @@
+from simverse.envs.farmtila.config import FarmtilaConfig
+import numpy as np
+from simverse.agent.sim_agent import SimAgent
+
+
+class FarmtilaEnv():
+    def __init__(self, config: FarmtilaConfig):
+        self.config = config
+
+        self.seed_grid = np.zeros((config.width, config.height))
+        self.owner_grid = np.full((config.width, config.height), -1)
+
+        # Agent class have their own position
+
+        self.agents: SimAgent = []
+
+        self.steps = 0
+    
+    def reset(self):
+        pass
+    
+    def step(self):
+        pass
+
+    def render(self):
+        pass
+
