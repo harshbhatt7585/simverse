68b2fbb926cb755393c707ea44fe7d67efd0485e
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-24 16:49:03 +0530

    build abstraction for stats

---
+++ src/simverse/agent/stats.py
@@ -0,0 +1,22 @@
+"Abstraction for agent stats"
+
+from abc import ABC, abstractmethod
+from simverse.agent.sim_agent import SimAgent
+
+class AgentStats(ABC):
+    @abstractmethod
+    def __init__(self, agent: SimAgent):
+        self.agent = agent
+        self.stats = {}
+
+    @abstractmethod
+    def reset(self) -> None:
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_stats(self) -> dict:
+        pass
