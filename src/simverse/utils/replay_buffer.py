4baed6ca966ec51f68995b1bd5e97dd50f53e037
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-25 18:10:06 +0530

    create a template for replay buffer

---
+++ src/simverse/utils/replay_buffer.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Dict, List
+
+
+@dataclass
+class Experience:
+    observation: Dict 
+    reward: List[float]
+    done: List[bool]
+    info: List[Dict]
+
+
+class ReplayBuffer:
+    def __init__(self, max_size: int = 10000):
+        self.max_size = max_size
+    
+    def add(self, experience: tuple) -> None:
+        pass
+
+    def sample(self, batch_size: int) -> List[Experience]:
+        pass
