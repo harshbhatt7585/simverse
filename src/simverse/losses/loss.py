ac9937f877947aae2770e09eebed417688100dac
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-25 19:56:43 +0530

    remove reduntant

--- src/simverse/losses/loss.py
+++
@@ -1,9 +0,0 @@
-"Abstract class for all losses."
-from abc import ABC, abstractmethod
-
-class Loss(ABC):
-    @abstractmethod
-    def __call__(self, *args, **kwargs) -> float:
-        pass
-
-    
