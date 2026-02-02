fd983e81eaeb42df1e6f39c23bebe193aa078887
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-02-02 23:52:30 +0530

    implement _run_worker

--- src/simverse/utils/worker.py
+++ src/simverse/utils/worker.py
@@ -1,9 +1,10 @@
 import multiprocessing as mp
 from dataclasses import dataclass
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from simverse.envs.farmtila.env import FarmtilaEnv
 from simverse.policies.simple import SimplePolicy
+from simverse.simulator import Simulator
 
 
 @dataclass
@@ -16,13 +17,20 @@ class WorkerConfig:
 class Worker:
     def __init__(self, config: WorkerConfig):
         self.config = config
-        self.env = FarmtilaEnv(config.env_config)
+        self.num_workers = config.num_workers
+        self.envs = {
+            worker_id: FarmtilaEnv(config.env_config)
+            for worker_id in range(self.config.num_workers)
+        }
         self.policy = SimplePolicy(config.policy_state)
-
-    def _build_envs(self, num_envs: int) -> List[FarmtilaEnv]:
-        return [FarmtilaEnv(self.config.env_config) for _ in range(num_envs)]
 
     def run(self):
         with mp.Pool(processes=self.config.num_workers) as pool:
             results = pool.map(self._run_worker, range(self.config.num_workers))
         return results
+
+    def _run_worker(self, worker_id: int) -> None:
+        env = self.envs[worker_id]
+        agents = self.env.agents
+        Simulator(env, agents, self.policy)
+        Simulator.run()
