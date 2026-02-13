a2e7dbce07aa172e662cf2851cc9afb58734b3f3
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-02-13 22:41:29 +0530

    remove navlink

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import './App.css'
 import RenderPage from './pages/RenderPage'
@@ -6,17 +6,6 @@ import RenderPage from './pages/RenderPage'
 function App() {
   return (
     <div className="route-shell">
-      <header className="route-header">
-        <nav className="route-nav" aria-label="Primary">
-          <NavLink
-            to="/render"
-            className={({ isActive }) => (isActive ? 'route-link active' : 'route-link')}
-          >
-            Render
-          </NavLink>
-        </nav>
-      </header>
-
       <Routes>
         <Route path="/render" element={<RenderPage />} />
         <Route path="*" element={<Navigate to="/render" replace />} />
